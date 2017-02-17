a88a673190d45aa14f58971ff21c2dc510103564
Gaharit <Gaharit@gmail.com>
2017-02-17 21:15:59 +0800

    1

---
+++ Seminar1/1 - TwoNumberSwap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1___TwoNumberSwap
+{
+    class Program
+    {
+
+        static void Swap(int a, int b)
+        {
+            a += b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("{0}, {1}", a, b);
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Print two numbers");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Swap(a, b);
+        }
+    }
+}
---
+++ Seminar1/3 - Time(NoCycles)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3___Time_NoCycles_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
